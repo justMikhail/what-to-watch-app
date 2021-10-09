7470d1b84476d71e51f581a30db687bc66d90443
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-10-10 02:33:06 +0300

    add basic routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {AppRoute} from '../../const/app-route';
+
 import MainPage from '../pages/main-page/main-page';
+import SignInPage from '../pages/sign-in-page/sign-in-page';
+import MyListPage from '../pages/my-list-page/my-list-page';
+import MoviePage from '../pages/movie-page/movie-page';
+import AddReviewPage from '../pages/add-review-page/add-review-page';
+import PlayerPage from '../pages/player-page/player-page';
+
 
 type PromoFilmProps  = {
   title: string;
@@ -8,11 +17,35 @@ type PromoFilmProps  = {
 
 function App({title, genre, year}: PromoFilmProps): JSX.Element {
   return (
-    <MainPage
-      title={title}
-      genre={genre}
-      year={year}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainPage
+            title={title}
+            genre={genre}
+            year={year}
+          />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignInPage />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyListPage />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignInPage />
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <MoviePage />
+        </Route>
+        <Route exact path={AppRoute.AddReview}>
+          <AddReviewPage />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <PlayerPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const/app-route.ts
@@ -0,0 +1,8 @@
+export enum AppRoute {
+  Main = '/',
+  SignIn = '/login',
+  MyList = 'mylist',
+  Film = '/film/:id',
+  AddReview = '/film/:id/review',
+  Player = '/player/:id'
+}
