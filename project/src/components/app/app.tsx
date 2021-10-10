26024003c3ff62800b818c9f1a40e398964f24ed
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-10-10 03:53:35 +0300

    use "PrivateRoute" for "MyList" and "AddReview"

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {AppRoute} from '../../const/app-route';
+import {AuthorizationStatus} from '../../const/const';
+import PrivateRoute from '../private-route/private-route';
 
 import MainPage from '../pages/main-page/main-page';
 import SignInPage from '../pages/sign-in-page/sign-in-page';
@@ -7,6 +9,7 @@ import MyListPage from '../pages/my-list-page/my-list-page';
 import MoviePage from '../pages/movie-page/movie-page';
 import AddReviewPage from '../pages/add-review-page/add-review-page';
 import PlayerPage from '../pages/player-page/player-page';
+import NotFoundPage from '../pages/not-found-page/not-found-page';
 
 
 type PromoFilmProps  = {
@@ -29,20 +32,26 @@ function App({title, genre, year}: PromoFilmProps): JSX.Element {
         <Route exact path={AppRoute.SIGN_IN}>
           <SignInPage />
         </Route>
-        <Route exact path={AppRoute.MY_LIST}>
-          <MyListPage />
-        </Route>
-        <Route exact path={AppRoute.SIGN_IN}>
-          <SignInPage />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.MY_LIST}
+          render={() => <MyListPage />}
+          authorizationStatus={AuthorizationStatus.NO_AUTH}
+        />
         <Route exact path={AppRoute.FILM}>
           <MoviePage />
         </Route>
-        <Route exact path={AppRoute.ADD_REVIEW}>
-          <AddReviewPage />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.ADD_REVIEW}
+          render={() => <AddReviewPage />}
+          authorizationStatus={AuthorizationStatus.NO_AUTH}
+        />
         <Route exact path={AppRoute.PLAYER}>
           <PlayerPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
         </Route>
       </Switch>
     </BrowserRouter>
