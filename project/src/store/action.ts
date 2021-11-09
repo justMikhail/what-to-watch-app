4db80f11d2e0144e1176b8351bc8fd9d7beaf649
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-09 05:14:55 +0300

    Refactoring actions(use "ReturnType")

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,12 +1,12 @@
-import {ActionType, SelectGenreAction, FilterFilmsBySelectedGenreActions} from '../types/actions-types';
+import {ActionType} from '../types/actions-types';
 import {MOCK_FILMS} from '../mocks/mock-films';
 
-export const selectGenre = (selectedGenre: string): SelectGenreAction => ({
+export const selectGenre = (selectedGenre: string) => ({
   type: ActionType.SelectGenre,
   payload: selectedGenre,
-});
+} as const);
 
-export const filterFilmsBySelectedGenre = (): FilterFilmsBySelectedGenreActions => ({
+export const filterFilmsBySelectedGenre = () => ({
   type: ActionType.FilterFilmsBySelectedGenre,
   payload: MOCK_FILMS,
-});
+} as const);
--- project/src/types/actions-types.ts
+++ project/src/types/actions-types.ts
@@ -1,20 +1,14 @@
-import {FilmType} from './film-type';
+import {
+  selectGenre,
+  filterFilmsBySelectedGenre
+} from '../store/action';
 
 export enum ActionType {
   SelectGenre = 'main/selectGenre',
   FilterFilmsBySelectedGenre = 'main/filterFilmsBySelectedGenre',
 }
 
-export type SelectGenreAction = {
-  type: ActionType.SelectGenre;
-  payload: string;
-}
-
-export type FilterFilmsBySelectedGenreActions = {
-  type: ActionType.FilterFilmsBySelectedGenre;
-  payload: FilmType[];
-}
-
 export type Actions =
-  | SelectGenreAction
-  | FilterFilmsBySelectedGenreActions;
+  | ReturnType<typeof selectGenre>
+  | ReturnType<typeof filterFilmsBySelectedGenre>
+
