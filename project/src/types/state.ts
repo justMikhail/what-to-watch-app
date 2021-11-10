521407b92ee229694667d01f1e624e84380c2289
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-10 22:04:04 +0300

    Update initialState types

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,6 +1,8 @@
 import {FilmType} from './film-type';
+import {AuthorizationStatus} from '../const/authorization-status';
 
 export type State = {
   selectedGenre: string,
   allFilms: FilmType[],
+  authorizationStatus: AuthorizationStatus,
 }
