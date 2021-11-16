7aa5b4a736e255c62a26a0aaf65c3dc57d396454
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-16 19:45:49 +0300

    Update "PrivateRoute" component (types)

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,5 +1,6 @@
 import {Route, Redirect, RouteProps} from 'react-router-dom';
 import {connect, ConnectedProps} from 'react-redux';
+import {History} from 'history';
 
 import {AppRoute} from '../../const/routs';
 import {AuthorizationStatus} from '../../const/authorization-status';
@@ -11,8 +12,12 @@ const mapStateToProps = ({authorizationStatus}: State) => ({
 
 const connector = connect(mapStateToProps);
 
+type RenderFuncProps = {
+  history: History<unknown>;
+}
+
 type PrivateRouteProps = RouteProps & {
-  render: () => JSX.Element;
+  render: (props: RenderFuncProps) => JSX.Element;
   authorizationStatus: AuthorizationStatus;
 }
 
@@ -26,9 +31,9 @@ function PrivateRoute(props: ConnectedComponentProps): JSX.Element {
     <Route
       exact={exact}
       path={path}
-      render={() => (
+      render={(routeProps) => (
         authorizationStatus === AuthorizationStatus.Auth
-          ? render()
+          ? render(routeProps)
           : <Redirect to={AppRoute.SignIn} />
       )}
     />
