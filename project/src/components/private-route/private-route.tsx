7c336c7623a82a60794af4c736bb5421f77dc6a2
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-16 19:41:34 +0300

    Update "PrivateRoute" component (connect to Redux)

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,13 +1,27 @@
 import {Route, Redirect, RouteProps} from 'react-router-dom';
+import {connect, ConnectedProps} from 'react-redux';
+
 import {AppRoute} from '../../const/routs';
 import {AuthorizationStatus} from '../../const/authorization-status';
+import {State} from '../../types/state';
+
+const mapStateToProps = ({authorizationStatus}: State) => ({
+  authorizationStatus,
+});
+
+const connector = connect(mapStateToProps);
 
 type PrivateRouteProps = RouteProps & {
   render: () => JSX.Element;
   authorizationStatus: AuthorizationStatus;
 }
 
-function PrivateRoute({exact, path, render, authorizationStatus} : PrivateRouteProps): JSX.Element {
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type ConnectedComponentProps = PropsFromRedux & PrivateRouteProps;
+
+function PrivateRoute(props: ConnectedComponentProps): JSX.Element {
+  const {exact, path, render, authorizationStatus} = props;
+
   return (
     <Route
       exact={exact}
@@ -21,4 +35,5 @@ function PrivateRoute({exact, path, render, authorizationStatus} : PrivateRouteP
   );
 }
 
-export default PrivateRoute;
+export {PrivateRoute};
+export default connector(PrivateRoute);
