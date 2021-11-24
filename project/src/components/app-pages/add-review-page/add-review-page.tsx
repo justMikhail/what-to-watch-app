878ab3cbded491f955d50ddb0f5d658787584d2d
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-24 22:19:50 +0300

    Add "currentFilm" data in "AddReviewPage"

--- project/src/components/app-pages/add-review-page/add-review-page.tsx
+++ project/src/components/app-pages/add-review-page/add-review-page.tsx
@@ -1,9 +1,10 @@
-import Logo from '../../logo/Logo';
-import FormForAddReview from '../../form-for-add-review/form-for-add-review';
 import {useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
-import {getAllFilmsData} from '../../../store/films-data-reducer/selectors';
 
+import {getAllFilmsData} from '../../../store/films-data-reducer/selectors';
+import Header from '../../header/header';
+import FormForAddReview from '../../form-for-add-review/form-for-add-review';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 function AddReviewPage(): JSX.Element {
   const params = useParams<{ id: string }>();
@@ -11,53 +12,32 @@ function AddReviewPage(): JSX.Element {
   const currentFilm = useSelector(getAllFilmsData).find((filmItem) => filmItem.id === filmId);
 
   const onPostHandler = (reviewData: {rating: number, comment: string}) => {
-    console.log(reviewData)
+    console.log(reviewData);
+  };
+
+  if (!currentFilm) {
+    return <NotFoundPage />;
   }
 
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
+
         <div className="film-card__bg">
           <img
-            src="img/bg-the-grand-budapest-hotel.jpg"
-            alt="The Grand Budapest Hotel"
+            src={currentFilm.backgroundImage}
+            alt={currentFilm.name}
           />
         </div>
+
         <h1 className="visually-hidden">WTW</h1>
-        <header className="page-header">
-          <Logo />
-          <nav className="breadcrumbs">
-            <ul className="breadcrumbs__list">
-              <li className="breadcrumbs__item">
-                <a href="film-page.html" className="breadcrumbs__link">
-                  The Grand Budapest Hotel
-                </a>
-              </li>
-              <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link">Add review</a>
-              </li>
-            </ul>
-          </nav>
-          <ul className="user-block">
-            <li className="user-block__item">
-              <div className="user-block__avatar">
-                <img
-                  src="img/avatar.jpg"
-                  alt="User avatar"
-                  width={63}
-                  height={63}
-                />
-              </div>
-            </li>
-            <li className="user-block__item">
-              <a className="user-block__link">Sign out</a>
-            </li>
-          </ul>
-        </header>
+
+        <Header />
+
         <div className="film-card__poster film-card__poster--small">
           <img
-            src="img/the-grand-budapest-hotel-poster.jpg"
-            alt="The Grand Budapest Hotel poster"
+            src={currentFilm.posterImage}
+            alt={currentFilm.name}
             width={218}
             height={327}
           />
@@ -69,6 +49,7 @@ function AddReviewPage(): JSX.Element {
       </div>
     </section>
   );
+
 }
 
 export default AddReviewPage;
