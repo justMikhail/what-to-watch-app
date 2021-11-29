fe1d4ae3da0a46a16cf413f6111eba85e89ca925
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-29 11:13:31 +0300

    Fix "FilmPage" component

--- project/src/components/app-pages/film-page/film-page.tsx
+++ project/src/components/app-pages/film-page/film-page.tsx
@@ -44,105 +44,101 @@ function FilmPage(): JSX.Element {
     return <Loader />;
   }
 
-  if (currentFilmData) {
-    const generatedAddReviewPagePath = generatePath(AppRoute.AddReview, {id: currentFilmData.id});
+  const generatedAddReviewPagePath = generatePath(AppRoute.AddReview, {id: currentFilmData.id});
 
-    const addToMyListButtonIcon = currentFilmData.isFavorite
-      ? AddToMyListBurronIcon.Favorite
-      : AddToMyListBurronIcon.NotFavorite;
+  const addToMyListButtonIcon = currentFilmData.isFavorite
+    ? AddToMyListBurronIcon.Favorite
+    : AddToMyListBurronIcon.NotFavorite;
 
-    const handlePlayButtonClick = () => {
-      if (authorizationStatus !== AuthorizationStatus.Auth) {
-        dispatch(redirectToRoute(AppRoute.SignIn));
-      }
-      dispatch(redirectToRoute(AppRoute.Player));
-    };
+  const handlePlayButtonClick = () => {
+    if (authorizationStatus !== AuthorizationStatus.Auth) {
+      dispatch(redirectToRoute(AppRoute.SignIn));
+    }
+    dispatch(redirectToRoute(AppRoute.Player));
+  };
 
-    const handleAddToMyListButtonClick = () => {
-      if (authorizationStatus !== AuthorizationStatus.Auth) {
-        dispatch(redirectToRoute(AppRoute.SignIn));
-      }
-      dispatch(postFilmIsFavoriteAction(currentFilmData.id, currentFilmData.isFavorite));
-    };
+  const handleAddToMyListButtonClick = () => {
+    if (authorizationStatus !== AuthorizationStatus.Auth) {
+      dispatch(redirectToRoute(AppRoute.SignIn));
+    }
+    dispatch(postFilmIsFavoriteAction(currentFilmData.id, currentFilmData.isFavorite));
+  };
 
-    const onAddReviewButtonClickHandler = () => {
-      dispatch(redirectToRoute(generatedAddReviewPagePath));
-    };
+  const onAddReviewButtonClickHandler = () => {
+    dispatch(redirectToRoute(generatedAddReviewPagePath));
+  };
 
-    return (
-      <>
-        <section className="film-card film-card--full">
-          <div className="film-card__hero">
-            <div className="film-card__bg">
+  return (
+    <>
+      <section className="film-card film-card--full">
+        <div className="film-card__hero">
+          <div className="film-card__bg">
+            <img
+              src={currentFilmData.backgroundImage}
+              alt={currentFilmData.name}
+            />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <Header filmCard/>
+
+          <div className="film-card__wrap">
+            <div className="film-card__desc">
+
+              <h2 className="film-card__title">{currentFilmData.name}</h2>
+              <p className="film-card__meta">
+                <span className="film-card__genre">{currentFilmData.genre}</span>
+                <span className="film-card__year">{currentFilmData.released}</span>
+              </p>
+
+              <div className="film-card__buttons">
+
+                <PrimaryButton buttonText="Play" onButtonClickHandler={handlePlayButtonClick}>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s" />
+                  </svg>
+                </PrimaryButton>
+
+                <PrimaryButton buttonText="My List" onButtonClickHandler={handleAddToMyListButtonClick}>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref={addToMyListButtonIcon} />
+                  </svg>
+                </PrimaryButton>
+
+                <PrimaryButton buttonText="Add review" onButtonClickHandler={onAddReviewButtonClickHandler}/>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div className="film-card__wrap film-card__translate-top">
+          <div className="film-card__info">
+            <div className="film-card__poster film-card__poster--big">
               <img
-                src={currentFilmData.backgroundImage}
+                src={currentFilmData.posterImage}
                 alt={currentFilmData.name}
+                width={218}
+                height={327}
               />
             </div>
 
-            <h1 className="visually-hidden">WTW</h1>
-
-            <Header filmCard/>
-
-            <div className="film-card__wrap">
-              <div className="film-card__desc">
-
-                <h2 className="film-card__title">{currentFilmData.name}</h2>
-                <p className="film-card__meta">
-                  <span className="film-card__genre">{currentFilmData.genre}</span>
-                  <span className="film-card__year">{currentFilmData.released}</span>
-                </p>
-
-                <div className="film-card__buttons">
-
-                  <PrimaryButton buttonText="Play" onButtonClickHandler={handlePlayButtonClick}>
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref="#play-s" />
-                    </svg>
-                  </PrimaryButton>
-
-                  <PrimaryButton buttonText="My List" onButtonClickHandler={handleAddToMyListButtonClick}>
-                    <svg viewBox="0 0 19 19" width="19" height="19">
-                      <use xlinkHref={addToMyListButtonIcon} />
-                    </svg>
-                  </PrimaryButton>
-
-                  <PrimaryButton buttonText="Add review" onButtonClickHandler={onAddReviewButtonClickHandler}/>
-
-                </div>
-
-              </div>
-            </div>
+            <FilmInfoTabs film={currentFilmData} />
           </div>
-
-          <div className="film-card__wrap film-card__translate-top">
-            <div className="film-card__info">
-              <div className="film-card__poster film-card__poster--big">
-                <img
-                  src={currentFilmData.posterImage}
-                  alt={currentFilmData.name}
-                  width={218}
-                  height={327}
-                />
-              </div>
-
-              <FilmInfoTabs film={currentFilmData} />
-            </div>
-          </div>
-        </section>
-
-        <div className="page-content">
-          <section className="catalog catalog--like-this">
-            <h2 className="catalog__title">More like this</h2>
-            <FilmsList filmsForRender={similarFilms} />
-          </section>
-          <Footer />
         </div>
-      </>
-    );
-  }
+      </section>
 
-  return <Loader />;
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+          <FilmsList filmsForRender={similarFilms} />
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default FilmPage;
